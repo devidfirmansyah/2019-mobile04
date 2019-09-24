1eef50ba4527d5a8e4f6c7760262a300426bc5c1
Devid <firmansyahdevid@gmail.com>
2019-09-24 18:11:41 +0700

    Perubahan pada Intent all

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -8,11 +8,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
-
+private TextView nama;
+private TextView output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
-    }
+        nama = findViewById(R.id.input_name);
+        output = findViewById(R.id.text_output);
+}
 
+    public void Submit(View view) {
+        output.setText(nama.getText().toString());
+    }
 }
--- Intent/app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
@@ -2,14 +2,43 @@ package id.ac.polinema.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import model.User;
 
 public class ParcelableActivity extends AppCompatActivity {
+    private EditText usernameInput;
+    private EditText nameInput;
+    private EditText ageInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parcelable);
+
+        usernameInput = findViewById(R.id.input_username);
+        nameInput = findViewById(R.id.input_name);
+        ageInput = findViewById(R.id.input_age);
+
+
     }
 
+    public void handleParcelable(View view) {
+        String username, name;
+        int age;
+        username = usernameInput.getText().toString().trim();
+        name = nameInput.getText().toString().trim();
+        age = Integer.parseInt(ageInput.getText().toString().trim());
+        User user = new User(username,name,age);
+        Intent intent = new Intent(this, ProfileParcelableActivity.class);
+        intent.putExtra("username", user.getUsername());
+        intent.putExtra("name", user.getName());
+        intent.putExtra("age", user.getAge());
+        startActivity(intent);
+
+
+    }
 }
